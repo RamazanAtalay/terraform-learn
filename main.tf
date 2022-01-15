b745ad5abc6dc6cd9ae41198a8e51db04eaec61c
Ramazan Atalay <ramazanatalay@gmail.com>
2022-01-15 00:44:18 -0500

    modules

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
-
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
     Name = "${var.env_prefix}-vpc"
   }
 }
-
 module "myapp-subnet" {
   source                 = "./modules/subnet"
   subnet_cidr_block      = var.subnet_cidr_block
@@ -17,7 +15,6 @@ module "myapp-subnet" {
   vpc_id                 = aws_vpc.myapp-vpc.id
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
-
 module "myapp-server" {
   source              = "./modules/webserver"
   vpc_id              = aws_vpc.myapp-vpc.id
